1f629ea5acf29013aa42eb9436f64e552177df9e
Blueibis <jennie.li.2011@gmail.com>
2017-05-21 14:57:57 -0400

    rover can read whole instructions. majority of add/delete changes from SiB

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,59 +1,82 @@
 class Rover
 
-  attr_accessor :read_instruction, :turn
+  attr_accessor :read_instruction, :turn  # => nil
 
   def initialize(x_position, y_position, direction)
-    @x_position = x_position
-    @y_position = y_position
-    @direction = direction
-  end
+    @x_position = x_position                         # => 1
+    @y_position = y_position                         # => 1
+    @direction = direction                           # => "N"
+  end                                                # => :initialize
 
   def read_instruction(instructions)
-      @input = instructions.split("")
-      print @input
-      @input.each do |order|
-        if order == "L" || order == "R"
-          turn
-        else
-          move
-        end
-      end
-  end
+      @input = instructions.split("")                # => ["M", "M", "M", "L", "L", "M", "R", "M"]
+          @input.each do |letter|                    # => ["M", "M", "M", "L", "L", "M", "R", "M"]
+            if letter == "L"                         # => false, false, false, true, true, false, false, false
+              turn("L")                              # => "W", "S"
+            elsif letter == "R"                      # => false, false, false, false, true, false
+              turn("R")                              # => "W"
+            elsif letter == "M"                      # => true, true, true, true, true
+              move                                   # => 2,    3,    4,    3,    0
+            else
+              puts "Error: Command does not exist."
+            end                                      # => 2, 3, 4, "W", "S", 3, "W", 0
+          end                                        # => ["M", "M", "M", "L", "L", "M", "R", "M"]
+  end                                                # => :read_instruction
 
-  def turn
-    @input.each do |letter|
-        case letter == "L"
-          when @direction == "N"
-            @direction = "W"
-          when @direction == "E"
-            @direction = "N"
-          when @direction == "S"
-            @direction = "E"
-          when @direction == "W"
-            @direction = "S"
-        end
-        case letter == "R"
-          when @direction == "N"
-            @direction = "E"
-          when @direction == "E"
-            @direction = "S"
-          when @direction == "S"
-            @direction = "W"
-          when @direction == "W"
-            @direction = "N"
-        end
-      end
-  end
+  def turn(letter)
+        if letter == "L"     # => true, true, false
+          set_direction_l    # => "W", "S"
+        elsif letter == "R"  # => true
+        set_direction_r      # => "W"
+        else
+        end                  # => "W", "S", "W"
+  end                        # => :turn
+
+
+  def set_direction_l
+    if @direction == "N"     # => true, false
+      @direction = "W"       # => "W"
+    elsif @direction == "W"  # => true
+      @direction = "S"       # => "S"
+    elsif @direction == "S"
+      @direction = "E"
+    else
+      @direction = "N"
+    end                      # => "W", "S"
+  end                        # => :set_direction_l
+
+  def set_direction_r
+    if @direction == "N"     # => false
+      @direction = "E"
+    elsif @direction == "E"  # => false
+      @direction = "S"
+    elsif @direction == "S"  # => true
+      @direction = "W"       # => "W"
+    else
+      @direction = "N"
+    end                      # => "W"
+  end                        # => :set_direction_r
 
   def move
-    
-  end
+    if @direction == "N"     # => true, true, true, false, false
+      @x_position += 1       # => 2, 3, 4
+    elsif @direction == "S"  # => true, false
+      @x_position -= 1       # => 3
+    elsif @direction == "E"  # => false
+      @y_position += 1
+    else
+      @y_position -= 1       # => 0
+    end                      # => 2, 3, 4, 3, 0
+ end                         # => :move
+
   def report
-    puts "x:#{@x_position} y:#{@y_position} #{@direction}"
-  end
+    puts "x:#{@x_position} y:#{@y_position} #{@direction}"  # => nil
+  end                                                       # => :report
 
-end
+end  # => :report
 
-rover1 = Rover.new(1, 1, "N")
-rover1.read_instruction("LRR")
-rover1.report
+rover1 = Rover.new(1, 1, "N")        # => #<Rover:0x005558ae8fe128 @x_position=1, @y_position=1, @direction="N">
+rover1.read_instruction("MMMLLMRM")  # => ["M", "M", "M", "L", "L", "M", "R", "M"]
+rover1.report                        # => nil
+
+# >> x:3 y:0 W
